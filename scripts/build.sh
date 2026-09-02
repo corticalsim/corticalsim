27fb2b15fd4b51819a87bcdd1b3de83d07748b9d
cantordust <43831101+cantordust@users.noreply.github.com>
2026-09-02 12:05:37 +0200

    Updated the build script. There is no directory traversal at all now, all directory arguments are built on the fly and passed to meson.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,57 +1,74 @@
 #!/bin/bash
 
 # Variables
-SCRIPT_DIR=`pwd`
-ROOT_DIR=`dirname "$SCRIPT_DIR"`
+# =========
+
+# This will extract an invariant path regardless of
+# where the script has been called from.
+SCRIPT_DIR="$(cd -- "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+
+# The repository root.
+ROOT_DIR=$(dirname "$SCRIPT_DIR")
+
+# Build type.
+# See https://mesonbuild.com/Running-Meson.html#configuring-the-build-directory
+# Possible values: "plain", "debug", "debugoptimized", "release"
 BUILD_TYPE="release"
-RECONFIGURE=""
-CLEAN=false
 
-# Move to the root directory (one up from this one)
-cd $ROOT_DIR
+# Aguments passed to `meson setup`.
+SETUP_ARGS=""
 
-# Get all arguments
+# Aguments passed to `meson compile`.
+COMPILE_ARGS=""
+
+# Default build directory.
+BUILD_DIR="$ROOT_DIR/build"
+
+# CLI arguments
+# =============
 ARG=( "${@}" )
 for i in ${ARG[@]}; do
-    if [ "$i" = "--reconfigure" ]; then
-        RECONFIGURE='--reconfigure'
-    fi
-    if [ "$i" = "--clean" ]; then
-        CLEAN=true
-    fi
-    if [ "$i" = "--buildtype" ]; then
-        shift 1
-        BUILD_TYPE="$1"
-    fi
+    case "$i" in
+
+        "--reconfigure")
+            SETUP_ARGS="$SETUP_ARGS --reconfigure"
+            ;;
+
+        "--builddir")
+            shift 1
+            BUILD_DIR="$1"
+            ;;
+
+        "--buildtype")
+            shift 1
+            BUILD_TYPE="$1"
+            SETUP_ARGS="$SETUP_ARGS --buildtype=$BUILD_TYPE"
+            ;;
+
+        "--clean")
+            COMPILE_ARGS="$COMPILE_ARGS --clean"
+            ;;
+    esac
     shift 1
 done
 
-BUILD_DIR="build/$BUILD_TYPE"
+# Create the build directory
+BUILD_DIR="$(readlink -m $BUILD_DIR)/$BUILD_TYPE"
+mkdir -p $BUILD_DIR
 echo ">>> Build directory: '$BUILD_DIR'"
 
-if [ $CLEAN == true ]; then
-    echo ">>> Cleaning..."
-    rm -rf $BUILD_DIR
-    echo ">>> Cleaning: done!"
-fi
+# Setup
+# =====
+echo ">>> Running setup..."
+SETUP_CMD=$(echo "meson setup $BUILD_DIR $ROOT_DIR $SETUP_ARGS" | xargs)
+echo ">>> Setup command: '$SETUP_CMD'"
+# $SETUP_CMD
+echo ">>> Running setup: done!"
 
-mkdir -p $BUILD_DIR
-
-# Install dependency wraps
-echo ">>> Installing dependencies..."
-meson wrap install eigen 5.0.1-1
-echo ">>> Installing dependencies: done!"
-
-# Configure
-echo ">>> Configuring the build..."
-CONF_CMD="meson setup $BUILD_DIR . --buildtype $BUILD_TYPE $RECONFIGURE"
-echo ">>> Configuration command: '$CONF_CMD'"
-$CONF_CMD
-echo ">>> Configuring the build: done!"
-
-# Compile
+# Compilation
+# ===========
 echo ">>> Compiling CorticalSim..."
-COMP_CMD="meson compile -C $BUILD_DIR"
-echo ">>> Compilation command: '$COMP_CMD'"
-$COMP_CMD
+COMPILATION_CMD=$(echo "meson compile -C $BUILD_DIR $COMPILE_ARGS" | xargs)
+echo ">>> Compilation command: '$COMPILATION_CMD'"
+# $COMPILATION_CMD
 echo ">>> Compiling CorticalSim: done!"
